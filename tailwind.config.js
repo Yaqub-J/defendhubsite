84befdae201eb1b510cbbfd435eb665b099c61a0
Yaqub-J <yaqubishaqjae@gmail.com>
2025-02-24 14:36:20 +0100

    First deployment

---
+++ tailwind.config.js
@@ -0,0 +1,81 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  darkMode: "class",
+  theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+
+    screens: {
+      xs: "450px",
+      // => @media (min-width: 450px) { ... }
+
+      sm: "575px",
+      // => @media (min-width: 576px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "992px",
+      // => @media (min-width: 992px) { ... }
+
+      xl: "1200px",
+      // => @media (min-width: 1200px) { ... }
+
+      "2xl": "1400px",
+      // => @media (min-width: 1400px) { ... }
+    },
+    extend: {
+      colors: {
+        current: "currentColor",
+        transparent: "transparent",
+        white: "#FFFFFF",
+        black: "#090204", 
+        dark: "#3A1521",
+        primary: "#8B0000",
+        yellow: "#FBB040",
+        "bg-color-dark": "#17050B",
+        "body-color": {
+          DEFAULT: "#564047",
+          dark: "#6B6064",
+        },
+        stroke: {
+          stroke: "#E3C1C2",
+          dark: "#482A34",
+        },
+        gray: {
+          ...colors.gray,
+          dark: "#130409",
+          light: "#FCDCDC",
+        },
+      },
+
+      boxShadow: {
+        signUp: "0px 5px 10px rgba(4, 10, 34, 0.2)",
+        one: "0px 2px 3px rgba(7, 7, 77, 0.05)",
+        two: "0px 5px 10px rgba(6, 8, 15, 0.1)",
+        three: "0px 5px 15px rgba(6, 8, 15, 0.05)",
+        sticky: "inset 0 -1px 0 0 rgba(0, 0, 0, 0.1)",
+        "sticky-dark": "inset 0 -1px 0 0 rgba(255, 255, 255, 0.1)",
+        "feature-2": "0px 10px 40px rgba(48, 86, 211, 0.12)",
+        submit: "0px 5px 20px rgba(4, 10, 34, 0.1)",
+        "submit-dark": "0px 5px 20px rgba(4, 10, 34, 0.1)",
+        btn: "0px 1px 2px rgba(4, 10, 34, 0.15)",
+        "btn-hover": "0px 1px 2px rgba(0, 0, 0, 0.15)",
+        "btn-light": "0px 1px 2px rgba(0, 0, 0, 0.1)",
+      },
+      dropShadow: {
+        three: "0px 5px 15px rgba(6, 8, 15, 0.05)",
+      },
+    },
+  },
+  plugins: [],
+};
